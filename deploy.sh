79dc7dc7d4206a882aeea3d230fbcca3415c8568
surajsaini95 <surajkumar90@rocketmail.com>
2020-04-22 01:38:41 +0530

    updated-deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -30,15 +30,12 @@
 ################################################################
 ################################################################
 #          Beginning of Main                                   #
-# JAR_LOCATION='/home/knoldus/Desktop/devops-training/Jenkins/project/akka-http-examples/target/scala-2.11'
 JAR_LOCATION='/home/knoldus/jenkins-slave/workspace/capstone-CI-CD_master/target/scala-2.11'
 JAR_NAME='akka-http-helloworld-assembly-1.0.jar'
-
 
 CURRENT_PID=$(ps aux | grep $JAR_NAME | cut -d' ' -f3 | head -n 1)
 echo "Killing currently running App with pid : $CURRENT_PID"
 kill $CURRENT_PID
-
 
 nohup java -jar $JAR_LOCATION/$JAR_NAME &
 echo "================================================"
